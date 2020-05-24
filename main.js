623ea69b5e5b5aaac42c465ddac6a28f9f432c76
MansiBhalotia <42296983+MansiBhalotia@users.noreply.github.com>
2020-05-24 16:23:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function(){
+
+	"use strict"
+
+	$('.slider').ripples({
+		resolution:512,
+		dropRadius: 20,
+		perturbance: 0.03,
+		interactive: true,
+	});
+
+	var typed = new Typed('.text', {
+  strings: ['I am <strong class=primary>MANSI BHALOTIA</strong>.', 'This Is A Project On <strong class=secondary>Front-End Development</strong>.'],
+  typespeed:0,
+  loop:true
+	});
+
+	$(window).scroll(function(){
+		var top=$(window).scrollTop();
+
+		if(top>70)
+		{
+			$('header').addClass('transparent-bg')
+		}
+		else
+		{
+			if($('header').hasClass('transparent-bg'))
+			{
+				$('header').removeClass('transparent-bg')
+			}
+		}
+	});
+});
